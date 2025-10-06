18c8114fd6d34c44df42adf97dad2e1a0a56b11c
lequocdat8272 <lequocdat8272@gmail.com>
2025-10-06 18:01:49 +0700

    Tab Home

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QFile>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QFile styleFile("style.qss");
+    if (styleFile.open(QFile::ReadOnly)) {
+        a.setStyleSheet(styleFile.readAll());
+    }
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}

b4b2138b1446fd65f5d712c54dd05c1531cc2cdf
lequocdat8272 <lequocdat8272@gmail.com>
2025-10-07 23:37:17 +0700

    quick fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QFile styleFile("style.qss");
+    QFile styleFile("resource/style.qss");
     if (styleFile.open(QFile::ReadOnly)) {
         a.setStyleSheet(styleFile.readAll());
     }
